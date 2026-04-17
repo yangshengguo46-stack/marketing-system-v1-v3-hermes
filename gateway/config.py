8c7fc2a4c1d90d20e306b4b5037e60de53ca65d7
youngDoo <1342088860@qq.com>
2026-04-17 16:59:25 +0800

    gateway cant add DingTalk platform
    gateway cant add DingTalk platform without key and secret

--- gateway/config.py
+++ gateway/config.py
@@ -307,6 +307,14 @@ class GatewayConfig:
             # QQBot uses extra dict for app credentials
             elif platform == Platform.QQBOT and config.extra.get("app_id") and config.extra.get("client_secret"):
                 connected.append(platform)
+            # DingTalk uses client_id/client_secret from config.extra or env vars
+            elif platform == Platform.DINGTALK and (
+                config.extra.get("client_id") or os.getenv("DINGTALK_CLIENT_ID")
+            ) and (
+                config.extra.get("client_secret") or os.getenv("DINGTALK_CLIENT_SECRET")
+            ):                                                                                                                                                                                    
+                connected.append(platform)
+        
         return connected
     
     def get_home_channel(self, platform: Platform) -> Optional[HomeChannel]:
