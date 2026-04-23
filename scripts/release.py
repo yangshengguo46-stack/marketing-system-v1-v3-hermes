ad6321577ecdf3dbdaf48ea05219e8525bd3d5cf
Teknium <teknium1@gmail.com>
2026-04-22 17:44:32 -0700

    chore(release): map brianclemens in AUTHOR_MAP

--- scripts/release.py
+++ scripts/release.py
@@ -357,6 +357,7 @@ AUTHOR_MAP = {
     "vrinek@hey.com": "vrinek",
     "268198004+xandersbell@users.noreply.github.com": "xandersbell",
     "somme4096@gmail.com": "Somme4096",
+    "brian@tiuxo.com": "brianclemens",
 }
 
 
