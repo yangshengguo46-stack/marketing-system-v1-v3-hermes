ff24b29c5f5f79a5eecf45dc60474348fb3483cb
Teknium <teknium1@gmail.com>
2026-04-24 15:15:41 -0700

    chore(release): map iRonin personal email to GitHub login

--- scripts/release.py
+++ scripts/release.py
@@ -68,6 +68,7 @@ AUTHOR_MAP = {
     "185121704+stablegenius49@users.noreply.github.com": "stablegenius49",
     "101283333+batuhankocyigit@users.noreply.github.com": "batuhankocyigit",
     "255305877+ismell0992-afk@users.noreply.github.com": "ismell0992-afk",
+    "cyprian@ironin.pl": "iRonin",
     "valdi.jorge@gmail.com": "jvcl",
     "q19dcp@gmail.com": "aj-nt",
     "francip@gmail.com": "francip",
