abc638d08bb740cdc532bd3dfa5300546707210b
Teknium <teknium1@gmail.com>
2026-04-24 03:01:43 -0700

    chore(release): add keiravoss94 AUTHOR_MAP entry

--- scripts/release.py
+++ scripts/release.py
@@ -60,6 +60,7 @@ AUTHOR_MAP = {
     "abner.the.foreman@agentmail.to": "Abnertheforeman",
     "harryykyle1@gmail.com": "hharry11",
     "kshitijk4poor@gmail.com": "kshitijk4poor",
+    "keira.voss94@gmail.com": "keiravoss94",
     "16443023+stablegenius49@users.noreply.github.com": "stablegenius49",
     "185121704+stablegenius49@users.noreply.github.com": "stablegenius49",
     "101283333+batuhankocyigit@users.noreply.github.com": "batuhankocyigit",
