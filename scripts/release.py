9f92b1059b1951eef7eebbf786a10671be8b1e44
Teknium <teknium1@gmail.com>
2026-04-24 16:08:17 -0700

    chore(release): map chenzeshi@live.com -> chen1749144759 in AUTHOR_MAP

--- scripts/release.py
+++ scripts/release.py
@@ -500,6 +500,7 @@ AUTHOR_MAP = {
     "zhangxicen@example.com": "zhangxicen",
     "codex@openai.invalid": "teknium1",
     "screenmachine@gmail.com": "teknium1",
+    "chenzeshi@live.com": "chen1749144759",
 }
 
 
