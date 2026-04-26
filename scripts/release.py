6163e03c973e21b261ceb440236dd3d4f7356a39
Teknium <teknium1@gmail.com>
2026-04-26 11:46:01 -0700

    chore(release): map zhiyanliu in AUTHOR_MAP

--- scripts/release.py
+++ scripts/release.py
@@ -118,6 +118,7 @@ AUTHOR_MAP = {
     "Mibayy@users.noreply.github.com": "Mibayy",
     "mibayy@users.noreply.github.com": "Mibayy",
     "135070653+sgaofen@users.noreply.github.com": "sgaofen",
+    "lzy.dev@gmail.com": "zhiyanliu",
     "nocoo@users.noreply.github.com": "nocoo",
     "30841158+n-WN@users.noreply.github.com": "n-WN",
     "tsuijinglei@gmail.com": "hiddenpuppy",
