5488e791213d19cc1ee5c6ae69b4ee60f214b99a
Teknium <127238744+teknium1@users.noreply.github.com>
2026-04-30 03:28:17 -0700

    chore(release): map Heltman email to username for AUTHOR_MAP

--- scripts/release.py
+++ scripts/release.py
@@ -618,6 +618,7 @@ AUTHOR_MAP = {
     "nfb0408@163.com": "ningfangbin",
     "164839249+Joseph19820124@users.noreply.github.com": "Joseph19820124",
     "rugved@lmstudio.ai": "rugvedS07",
+    "44333070+Heltman@users.noreply.github.com": "Heltman",
 }
 
 
