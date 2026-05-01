8bab43736e186a9190f145b72654627ed4b7cb84
Teknium <127238744+teknium1@users.noreply.github.com>
2026-04-30 19:52:58 -0700

    chore(release): add quocanh261997 to AUTHOR_MAP

--- scripts/release.py
+++ scripts/release.py
@@ -113,6 +113,7 @@ AUTHOR_MAP = {
     "foxion37@gmail.com": "foxion37",
     "bloodcarter@gmail.com": "bloodcarter",
     "scott@scotttrinh.com": "scotttrinh",
+    "quocanh261997@gmail.com": "quocanh261997",
     # contributors (from noreply pattern)
     "david.vv@icloud.com": "davidvv",
     "wangqiang@wangqiangdeMac-mini.local": "xiaoqiang243",
