6b281e50f91e2847db7d56f6b9a098d316892f58
Teknium <127238744+teknium1@users.noreply.github.com>
2026-05-09 14:48:13 -0700

    chore: add kidonng to AUTHOR_MAP

--- scripts/release.py
+++ scripts/release.py
@@ -64,6 +64,7 @@ AUTHOR_MAP = {
     "ytchen0719@gmail.com": "liquidchen",
     "am@studio1.tailb672fe.ts.net": "subtract0",
     "axmaiqiu@gmail.com": "qWaitCrypto",
+    "44045911+kidonng@users.noreply.github.com": "kidonng",
     "maksesipov@gmail.com": "Qwinty",
     "denisamania@gmail.com": "CalmProton",
     "308068+mbac@users.noreply.github.com": "mbac",
