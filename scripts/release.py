cfc2d34dc95a81a3fa8776965442204361520850
Teknium1 <127238744+teknium1@users.noreply.github.com>
2026-05-11 16:50:39 -0700

    chore(release): add AUTHOR_MAP entry for ahmedbadr3

--- scripts/release.py
+++ scripts/release.py
@@ -53,6 +53,7 @@ AUTHOR_MAP = {
     "421774554@qq.com": "wuli666",
     "harish.kukreja@gmail.com": "counterposition",
     "1046611633@qq.com": "zhengyn0001",
+    "ahmed@abadr.net": "ahmedbadr3",
     "cleo@edaphic.xyz": "curiouscleo",
     "hirokazu.ogawa@kwansei.ac.jp": "hrkzogw",
     "datapod.k@gmail.com": "dandacompany",
