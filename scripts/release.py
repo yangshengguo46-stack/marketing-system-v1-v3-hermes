c99e4ab12f2ef20b5489488799457b0db5b5c1b9
teknium1 <127238744+teknium1@users.noreply.github.com>
2026-05-23 16:16:46 -0700

    chore: map Glucksberg noreply email in AUTHOR_MAP

--- scripts/release.py
+++ scripts/release.py
@@ -85,6 +85,7 @@ AUTHOR_MAP = {
     "yanglongwei06@gmail.com": "Alex-yang00",
     "teknium@nousresearch.com": "teknium1",
     "markuscontasul@gmail.com": "Glucksberg",
+    "80581902+Glucksberg@users.noreply.github.com": "Glucksberg",
     "piyushvp1@gmail.com": "thelumiereguy",
     "dskwelmcy@163.com": "dskwe",
     "421774554@qq.com": "wuli666",
