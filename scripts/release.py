56331ad1875eaabce7b35fd97ac39f4b30cf8ddd
Teknium <127238744+teknium1@users.noreply.github.com>
2026-05-23 23:11:37 -0700

    chore: map pnascimento9596@gmail.com for PR #31235 salvage

--- scripts/release.py
+++ scripts/release.py
@@ -88,6 +88,7 @@ AUTHOR_MAP = {
     "markuscontasul@gmail.com": "Glucksberg",
     "80581902+Glucksberg@users.noreply.github.com": "Glucksberg",
     "piyushvp1@gmail.com": "thelumiereguy",
+    "pnascimento9596@gmail.com": "pnascimento9596",
     "dskwelmcy@163.com": "dskwe",
     "421774554@qq.com": "wuli666",
     "twebefy@gmail.com": "tw2818",
