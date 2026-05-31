a59265a5c3fb4ec907859b7553a1ccb746dd2eb5
Trevin Chow <trevin@trevinchow.com>
2026-05-30 17:52:20 -0700

    chore: add contributor attribution mapping

--- scripts/release.py
+++ scripts/release.py
@@ -48,6 +48,7 @@ AUTHOR_MAP = {
     "ben.bartholomew@vectorize.io": "benfrank241",
     "74339271+SaguaroDev@users.noreply.github.com": "SaguaroDev",
     "subw3@mail2.sysu.edu.cn": "Subway2023",
+    "trevin@trevinchow.com": "tmchow",
     "zhipengli@thebrainly.ai": "a1245582339",
     "mathijs.vd.hurk@gmail.com": "mathijsvandenhurk",
     "drpelagik@gmail.com": "SeaXen",
