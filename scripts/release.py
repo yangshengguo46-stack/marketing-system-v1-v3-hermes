81efc018ad300745618ff1b9a38b4dd505a2f6e0
teknium1 <127238744+teknium1@users.noreply.github.com>
2026-05-31 05:49:41 -0700

    chore: map fesalfayed author email for release notes

--- scripts/release.py
+++ scripts/release.py
@@ -80,6 +80,7 @@ AUTHOR_MAP = {
     "interstellar.consulting@gmail.com": "Interstellar-code",
     "33978413+Interstellar-code@users.noreply.github.com": "Interstellar-code",
     "tillfalko@gmail.com": "tillfalko",
+    "hi@fesalfayed.com": "fesalfayed",
     # teknium (multiple emails)
     "teknium1@gmail.com": "teknium1",
     "kenyon1977@gmail.com": "kenyonxu",
