d20ce6bb7088434650f67be42dbf1fe412781d62
Teknium <127238744+teknium1@users.noreply.github.com>
2026-06-11 22:58:34 -0700

    chore: add kdunn926 to AUTHOR_MAP

--- scripts/release.py
+++ scripts/release.py
@@ -76,6 +76,7 @@ AUTHOR_MAP = {
     "129007007+HeLLGURD@users.noreply.github.com": "HeLLGURD",
     "290859878+synapsesx@users.noreply.github.com": "synapsesx",
     "dirtyren@users.noreply.github.com": "dirtyren",
+    "kdunn926@gmail.com": "kdunn926",
     "mvanhorn@MacBook-Pro.local": "mvanhorn",
     "470766206@qq.com": "youjunxiaji",
     "mharris@parallel.ai": "NormallyGaussian",
