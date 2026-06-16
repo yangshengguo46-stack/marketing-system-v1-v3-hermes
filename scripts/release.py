fc6a19ea1bb735ded55e45f77f196f9693c0ecad
teknium1 <127238744+teknium1@users.noreply.github.com>
2026-06-16 11:53:08 -0700

    chore(release): AUTHOR_MAP entry for x1erra (Sierra)

--- scripts/release.py
+++ scripts/release.py
@@ -91,6 +91,7 @@ AUTHOR_MAP = {
     "290859878+synapsesx@users.noreply.github.com": "synapsesx",
     "157689911+itsflownium@users.noreply.github.com": "itsflownium",
     "dirtyren@users.noreply.github.com": "dirtyren",
+    "stevenn.damatoo@gmail.com": "x1erra",
     "evansrory@gmail.com": "zimigit2020",
     "237263164+ft-ioxcs@users.noreply.github.com": "ft-ioxcs",
     "tharushkadinujaya05@gmail.com": "0xneobyte",
