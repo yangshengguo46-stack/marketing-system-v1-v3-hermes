230b7adfbebd87fc16905d0d41e07835010ab531
kshitijk4poor <82637225+kshitijk4poor@users.noreply.github.com>
2026-06-21 12:21:41 +0530

    chore: add szzhoujiarui-sketch and rayjun to AUTHOR_MAP
    Salvage co-authors of the cron model.default fix.

--- scripts/release.py
+++ scripts/release.py
@@ -45,6 +45,8 @@ ACP_REGISTRY_MANIFEST = REPO_ROOT / "acp_registry" / "agent.json"
 
 # Auto-extracted from noreply emails + manual overrides
 AUTHOR_MAP = {
+    "szzhoujiarui@gmail.com": "szzhoujiarui-sketch",  # cron model.default salvage co-author (#45550)
+    "rayjun0412@gmail.com": "rayjun",  # cron model.default salvage co-author (#43952)
     "izumi0uu@gmail.com": "izumi0uu",  # PR #49544 salvage (native rich reply echo; #49534)
     "w31rdm4ch1n3z@protonmail.com": "w31rdm4ch1nZ",
     "xtpeeps@gmail.com": "x7peeps",
